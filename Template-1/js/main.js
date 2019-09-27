fbde9303e77b509a36d75422b1677d720c1f10ef
abc <abc@example.com>
2019-09-27 23:42:50 +0200

    modified

--- Template-1/js/main.js
+++ Template-1/js/main.js
@@ -20,8 +20,44 @@ $(window).scroll(function() {
         $(".navbar .nav-link").css("color", "#000")
     } else {
         
-        $(".navbar").css('backgroundColor', 'transparent')
+         $(".navbar").css('backgroundColor', 'transparent')
         $(".navbar .navbar-brand img").attr("src", "images/logo-light.png")
         $(".navbar .nav-link").css("color", "#FFF")
     }
+    
+    if (scrollTop > 1200) {
+        
+        $('#upBtn').fadeIn(500)
+    } else {
+        
+        $('#upBtn').fadeOut(500);
+    }
 })
+
+$('#upBtn').click(function() {
+    
+    $('body', 'html').animate({scrollTop: 0}, 1000)
+})
+
+$('.navbar a').click(function() {
+    
+    let href = $(this).attr('href'),
+        linkOffset = $(href).offset().top;
+    
+    $('body').animate({scrollTop: linkOffset}, 500)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
